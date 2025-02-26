46c01f964d0eef543cccad29df0ed0529f0c2d08
junhee97 <rlawnsgml97@naver.com>
2025-02-26 17:44:12 +0900

    day 03

--- MyJavaBook/src/Ch05/C06LogicArith.java
+++ MyJavaBook/src/Ch05/C06LogicArith.java
@@ -8,7 +8,7 @@ public class C06LogicArith {
 		a = 1;
 		b = 1;
 		c = (++a > 0) || (++b > 0);
-		System.out.printf("a = %d , b = %d c = %s\n", a, b, c); // 2 2 t
+		System.out.printf("a = %d , b = %d c = %s\n", a, b, c); // 2 1 t
 
 		a = 1;
 		b = 1;
